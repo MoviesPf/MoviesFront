ffeed58ea976b870d4ac1a5913b80f294b77a607
joseu <joseu@DESKTOP-4GF1JQ4>
2023-08-31 03:30:49 -0500

    css

---
+++ src/views/Home/Home.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { NavBar } from '../../Components/NavBar/NavBar';
+import { Genders } from '../../Components/Genders/Genres';
+import Platforms from '../../Components/Platforms/Platforms';
+import { Cards } from '../../Components/Cards/Cards';
+import { getAllMovies } from '../../Redux/actions';
+import css from './Home.module.css';
+
+export const Home = () => {
+  const dispatch = useDispatch();
+  const movies = useSelector((state) => state.movies);
+
+  useEffect(() => {
+    dispatch(getAllMovies());
+  }, [dispatch]);
+
+  return (
+    <div className={css.background}>
+      <NavBar />
+      <div className={css.cover}>
+
+      </div>
+      <Genders />
+      <Platforms />
+      <Cards movies={movies} />
+    </div>
+  );
+};
