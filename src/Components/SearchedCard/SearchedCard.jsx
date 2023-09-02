aa04be3ae0d1d746b3afc68aab036668d983035b
CabreraBrian <cabrerabriantbj@gmail.com>
2023-09-02 20:20:53 -0300

    commit search programs

---
+++ src/Components/SearchedCard/SearchedCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import css from './SearchedCard.module.css';
+import { Link } from "react-router-dom";
+
+export const SearchedCard = ({ program }) => {
+  return (
+    <div className={css.container}>
+      <Link className={css.link} to={`/detail/${program.id}`}>
+        <div className={css.allInfo}>
+          <div className={css.imagen}>
+            <img className={css.img} src={program.poster} alt={program.title} />
+          </div>
+          <div className={css.titleOverview}>
+            <h1>{program.title}</h1>
+            <p>{program.overview}</p>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+};
