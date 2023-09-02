26fffee35f46b61196d0607b53b1eea0cd1b7288
dannagonim <dannagonim@gmail.com>
2023-09-02 09:50:12 -0300

    cambios q no funcionan

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import css from './Card.module.css';
+import { Link } from "react-router-dom";
 
 export const Card = ({ poster, title }) => {
   return (
     <div className="card">
-      <img src={poster} alt={title} />
+      <img src={program.poster} alt={program.title} />
     </div>
   );
 };
