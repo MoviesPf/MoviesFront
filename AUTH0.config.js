d0d4738d91901a483ef84ae9cf4f1d5e43e5c4d4
Marian-Sofia <sofiagutierrez0031@gmail.com>
2023-09-04 11:51:57 -0500

    auth0

---
+++ AUTH0.config.js
@@ -0,0 +1,2 @@
+export const AUTH0_DOMAIN = 'dev-j0h4ropblcnlelpt.us.auth0.com';
+export const AUTH0_CLIENT_ID = 'JYvW5qdiPsV6HN4k7cBUoFmYJgHVTtAZ';
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import store from './Redux/store.js'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './Redux/store.js';
+import { Auth0Provider } from '@auth0/auth0-react';
+import { AUTH0_DOMAIN, AUTH0_CLIENT_ID } from '../AUTH0.config.js';
 
+// el AuthPrvider es para la configuracion de AUTH0
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Auth0Provider
+      domain={AUTH0_DOMAIN}
+      clientId={AUTH0_CLIENT_ID}
+      redirectUri={window.location.origin}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Auth0Provider>
   </Provider>
-)
+);
