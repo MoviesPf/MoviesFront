b01bde91e2b62fed805d98cae244f09d324bf3b0
CabreraBrian <cabrerabriantbj@gmail.com>
2023-09-05 02:53:57 -0300

    commit varios cambios visuales como hovers, gif de carga y eliminacion de archivos no utilzados

--- src/Components/SearchedCard/SearchedCard.jsx
+++ src/Components/SearchedCard/SearchedCard.jsx
@@ -5,30 +5,38 @@ import defaultImg from "../../assets/defaultMovie.png"
 
 export const SearchedCard = ({ program }) => {
   return (
-    <div className={css.container}>
       <Link className={css.link} to={`/detail/${program.id}`}>
         <div className={css.allInfo}>
+
           <div className={css.imagen}>
-        { program.poster === "https://image.tmdb.org/t/p/w500null" 
-        ? 
-        <img className={css.img} src={defaultImg}  alt={program.title}/>
-        : 
-        <img className={css.img} src={program.poster} alt={program.title} />
-        }
+            { program.poster === "https://image.tmdb.org/t/p/w500null" 
+            ? <img className={css.img} src={defaultImg}  alt={program.title}/>
+            : <img className={css.img} src={program.poster} alt={program.title} />
+            }
           </div>
-          <div className={css.titleOverview}>
+
+          <div className={css.data}>
             <h1>{program.title}</h1>
-            <p>{program.overview}</p>
-            <div className={css.genres}>
+            <div className={css.overview}>
+              <p>{program.overview}</p>
+            </div>
+            <div className={css.items}>
               {
                 program.Genres.map((genre) => {
                   return <p key={genre.name}>{genre.name}</p>
                 })
               }
             </div>
+            <div className={css.items}>
+              {
+                program.Platforms.map((platform) => {
+                  return <p ket={platform.name}>{platform.name}</p>
+                })
+              }
+            </div>
           </div>
+
         </div>
       </Link>
-    </div>
   );
 };
