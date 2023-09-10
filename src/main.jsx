3b6664bbce89aec6d0f18bdfe5a27a1d95672fb2
MoviesPf <142855170+MoviesPf@users.noreply.github.com>
2023-09-10 17:12:17 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { store } from './Redux/store.js';
+import store from './Redux/store.js';
 
 // el AuthPrvider es para la configuracion de AUTH0
 ReactDOM.createRoot(document.getElementById('root')).render(
