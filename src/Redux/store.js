b1f6cc8d5d15174ccc1e36b4e5b5f486cfdf669d
joseu <joseu@DESKTOP-4GF1JQ4>
2023-09-09 23:13:09 -0500

    .

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,24 +1,12 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunkMiddleware from "redux-thunk";
 import reducer from "./reducer";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage"
 
 const composeEnhacer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 //linea sirve para conectar nuestra App con la extension Redux Devtools del navegador.
-
-const persistConfig = {
-    key: 'root',
-    storage,
-}
-
-const persistedReducer = persistReducer(persistConfig, reducer)
-
-
-export let store = createStore(
-    persistedReducer,
+const store = createStore(
+    reducer,
     composeEnhacer(applyMiddleware(thunkMiddleware))
     );
-export let persistor = persistStore(store);
 
-
+export default store;
