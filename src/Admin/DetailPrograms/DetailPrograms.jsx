b17426c30fdba4ca1e1cefee8cd5c931f5b8cf18
jaider-nieto <jaiderlolxd@gmail.com>
2023-09-11 00:40:15 -0500

    s

---
+++ src/Admin/DetailPrograms/DetailPrograms.jsx
@@ -0,0 +1,11 @@
+import style from './DetailPrograms.module.css';
+
+const DetailPrograms = () => {
+  return (
+    <div>
+      <h1>DetailPrograms</h1>
+    </div>
+  );
+};
+
+export default DetailPrograms;
---
+++ src/Admin/DetailUsers/DetailUsers.jsx
@@ -0,0 +1,11 @@
+import style from './DetailUsers.module.css';
+
+const DetailUsers = () => {
+  return (
+    <div>
+      <h1>DetailUsers</h1>
+    </div>
+  );
+};
+
+export default DetailUsers;
