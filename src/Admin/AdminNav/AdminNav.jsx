1b784091e00a8d8c4563cbe67227ed6b33414482
jaider-nieto <jaiderlolxd@gmail.com>
2023-09-11 01:19:55 -0500

    s

--- src/Admin/AdminNav/AdminNav.jsx
+++ src/Admin/AdminNav/AdminNav.jsx
@@ -1,4 +1,4 @@
-import style from './Nav.module.css'
+import style from './AdminNav.module.css'
 
 const Nav = () => {
   return (
---
+++ src/Admin/index.js
@@ -0,0 +1,23 @@
+import DetailPrograms from './DetailPrograms/DetailPrograms';
+import DetailReviews from './DetailReviews/DetailReviews';
+import DetailUsers from './DetailUsers/DetailUsers';
+import Donations from './Donations/Donations';
+import Programs from './Programs/Programs';
+import Reviews from './Reviews/Reviews';
+import Users from './Users/Users';
+import Start from './Start/Start';
+import Form from './Form/Form';
+import Nav from './AdminNav/AdminNav';
+
+export {
+  DetailPrograms,
+  DetailReviews,
+  DetailUsers,
+  Donations,
+  Form,
+  Nav,
+  Programs,
+  Reviews,
+  Start,
+  Users
+};
