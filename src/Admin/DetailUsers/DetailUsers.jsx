8ff22290a0f99a763610e30d536ac25cd7e1daf8
jaider-nieto <jaiderlolxd@gmail.com>
2023-09-14 20:05:11 -0500

    s

--- src/Admin/DetailUsers/DetailUsers.jsx
+++ src/Admin/DetailUsers/DetailUsers.jsx
@@ -1,9 +1,17 @@
+import { useSelector } from 'react-redux';
 import style from './DetailUsers.module.css';
 
 const DetailUsers = () => {
+  const user = useSelector(select => select.userId) 
   return (
-    <div>
-      <h1>DetailUsers</h1>
+    <div className={style.container}>
+      <h1>Detail of {user.name}</h1>
+      <h1>{user.id}</h1>
+      <h1>{user.nickname}</h1>
+      <h1>{user.email}</h1>
+      <h1>{user.Reviews}</h1>
+      <h1>{user.banned}</h1>
+      <h1>{user.avatar}</h1>
     </div>
   );
 };
