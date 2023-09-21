a7b76104b159fde5b197806f2a1a1b00cbdf6d3b
DeepGlowing <julihuel@hotmail.com>
2023-09-20 22:35:57 -0300

    Cambio de estilos en donation

--- src/views/Donation/Donation.jsx
+++ src/views/Donation/Donation.jsx
@@ -1,21 +1,11 @@
-import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useLocalStorage } from '../../utils/useLocalStorage';
-
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { initiatePayment, selectDonationOption } from '../../Redux/actions';
 import { NavBar } from '../../Components/NavBar/NavBar';
 import css from './Donations.module.css';
 
 const Donations = () => {
-  const user = JSON.parse(localStorage.getItem("userStorage"));
-  const [userStorage, setUserStorage] = useLocalStorage('userStorage', {});
-
-  console.log(user)
-
   const dispatch = useDispatch();
-  const navigate = useNavigate();
-
   const selectedOption = useSelector((state) => state.selectedOption);
   const [sandboxInitPoint, setSandboxInitPoint] = useState('');
   const [successMessage, setSuccessMessage] = useState('');
@@ -27,7 +17,6 @@ const Donations = () => {
   const handleDonation = async () => {
     if (selectedOption) {
       const donationData = {
-        userId: user.id,
         title: 'Donation',
         price: selectedOption,
         quantity: 1
@@ -46,20 +35,16 @@ const Donations = () => {
       }
     }
   };
-  
+
   // obtengo el mensaje de éxito de los query params
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
     const successMsg = urlParams.get('message');
-    
+
     // muestro el mensaje de éxito si está presente
     if (successMsg) {
       console.log('Mensaje de éxito recibido:', successMsg);
       setSuccessMessage(successMsg);
-      setUserStorage({
-        ...user,
-        donator: true,
-      })
     }
   }, []);
 
@@ -77,13 +62,11 @@ const Donations = () => {
       <div className={css.txt}>
         <h1 className={css.title}>Help us keep the service free!</h1>
       </div>
+      <div className={css.content}>
+        <h2 className={css.subtitle}>Make your donation</h2>
 
-        { user?.id ? (
-          <div className={css.content}>
-          <h2 className={css.subtitle}>Make your donation</h2>
-
-          <div className={css.cards}>
-            <div className={css.card}>
+        <div className={css.cards}>
+          <div className={css.card}>
               <input
                 type='radio'
                 name='donationOption'
@@ -93,84 +76,42 @@ const Donations = () => {
                 onChange={() => handleOptionSelect(10)}
               />
               <h2 className={css.monto}>10$</h2>
-            </div>
-
-            <div className={css.card}>
-              <label>
-                <input
-                  type='radio'
-                  name='donationOption'
-                  className={css.chckMark}
-                  value={30}
-                  checked={selectedOption === 30}
-                  onChange={() => handleOptionSelect(30)}
-                />
-                <h2 className={css.monto}>30$</h2>
-              </label>
-            </div>
-
-            <div className={css.card}>
-              <label>
-                <input
-                  type='radio'
-                  name='donationOption'
-                  className={css.chckMark}
-                  value={50}
-                  checked={selectedOption === 50}
-                  onChange={() => handleOptionSelect(50)}
-                />
-                <h2 className={css.monto}>50$</h2>
-              </label>
-            </div>
-          </div>
-          <button className={css.btnDono} onClick={handleDonation}>
-            Donate
-          </button>
-        </div>
-      ) : (
-        <div className={css.content}>
-          <h2 className={css.subtitle}>Sign in to donate!!</h2>
-          <div className={css.cards}>
-            <div className={css.card}>
-              <label>
-                <h2 className={css.monto}>10$</h2>
-              </label>
-            </div>
-
-            <div className={css.card}>
-              <label>
-                <h2 className={css.monto}>30$</h2>
-              </label>
-            </div>
-
-            <div className={css.card}>
-              <label>
-                <h2 className={css.monto}>50$</h2>
-              </label>
-            </div>
           </div>
 
-          <div className={css.noLoginMessage}>
-            <div className={css.buttons}>
-              <button
-                className={css.btnDono}
-                onClick={() => navigate('/login')}
-              >
-                Login
-              </button>
-              <button
-                className={css.btnDono}
-                onClick={() => navigate('/signin')}
-              >
-                Create Acount
-              </button>
-            </div>
+          <div className={css.card}>
+            <label>
+              <input
+                type='radio'
+                name='donationOption'
+                className={css.chckMark}
+                value={30}
+                checked={selectedOption === 30}
+                onChange={() => handleOptionSelect(30)}
+              />
+              <h2 className={css.monto}>30$</h2>
+            </label>
+          </div>
+
+          <div className={css.card}>
+            <label>
+              <input
+                type='radio'
+                name='donationOption'
+                className={css.chckMark}
+                value={50}
+                checked={selectedOption === 50}
+                onChange={() => handleOptionSelect(50)}
+              />
+              <h2 className={css.monto}>50$</h2>
+            </label>
           </div>
         </div>
-      )}
+        <button className={css.btnDono} onClick={handleDonation}>Donate</button>
+      </div>
+
       {successMessage && (
         <div className={css.donationSuccess}>
-          <span>{successMessage}</span>
+          <span>donation successfully</span>
         </div>
       )}
       
